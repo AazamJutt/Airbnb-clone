6b4f5ac4d7e54921784fd04ae9cb7db9c385a741
Aazam Jutt <m.aazasultan@gmail.com>
2022-02-12 14:40:34 +0500

    locations page init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Locations from "./pages/Home/Locations";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />\
+          <Route path="/" element={<Home />} />
+          <Route path="/Locations" element={<Locations />} />
         </Routes>
       </BrowserRouter>
     </div>
